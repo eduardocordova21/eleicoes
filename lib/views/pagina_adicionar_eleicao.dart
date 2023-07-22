4a8a1415b0695df5294228e142ae8c51c6a50a5a
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2023-07-22 02:30:05 -0300

    Ajustando telas com os widgets criados

--- lib/views/pagina_adicionar_eleicao.dart
+++ lib/views/pagina_adicionar_eleicao.dart
@@ -1,39 +1,16 @@
+import 'package:eleicoes/widgets/form_adicionar_eleicao.dart';
 import 'package:flutter/material.dart';
 
-class PaginaAdicionarEleicao extends StatefulWidget {
+import '../widgets/app_bar_custom.dart';
+
+class PaginaAdicionarEleicao extends StatelessWidget {
   const PaginaAdicionarEleicao({Key? key}) : super(key: key);
 
   @override
-  State<PaginaAdicionarEleicao> createState() => _PaginaAdicionarEleicaoState();
-}
-
-class _PaginaAdicionarEleicaoState extends State<PaginaAdicionarEleicao> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("Adicionar uma eleição"),
-      ),
-      body: const Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Icon(Icons.add_task, size: 100.00, color: Colors.indigo),
-            ),
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Text(
-                "Vamos começar selecionando a eleição:",
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-          ],
-        ),
-      ),
+    return const Scaffold(
+      appBar: AppBarCustom(title: "Adicionar uma Eleição"),
+      body: FormAdicionarEleicao(),
     );
   }
 }
